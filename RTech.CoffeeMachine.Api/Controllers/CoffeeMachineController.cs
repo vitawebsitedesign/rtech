9e4a01a8b7b17df69d36e9f167247ed2a439655a
michael nguyen <3821810+vitawebsitedesign@users.noreply.github.com>
2023-02-11 20:16:44 +1100

    initial commit. Add local failing tests (tdd), bare-bones controller, xunit & shouldly packages

---
+++ RTech.CoffeeMachine.Api/Controllers/CoffeeMachineController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RTech.CoffeeMachine.Api.Controllers;
+
+[ApiController]
+[Route("")]
+public class CoffeeMachineController : ControllerBase
+{
+	public CoffeeMachineController()
+	{
+	}
+
+	[HttpGet("brew-coffee")]
+	public IActionResult GetBrewStatus()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ RTech.CoffeeMachine.Api/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+
+var app = builder.Build();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
